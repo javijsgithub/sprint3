d434f941ad91a3101ff21341fc084241d8c6b44d
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2023-12-11 12:13:37 +0100

    exercise 3 completado

--- src/films.js
+++ src/films.js
@@ -19,8 +19,13 @@ function getMoviesFromDirector(array, director) {
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+
+  let filterDirector = movies.filter(movie => movie.director === 'Quentin Tarantino');
+  let result = filterDirector.reduce((total, filter) => total + filter.score, 0);
+  console.log("EXERCISE 3 ->", "Media de las notas de las 5 peliculas:", result.toFixed(2) / 5);// no entiendo porque no salen los dos decimales en el resultado <8.4>
+};
+moviesAverageOfDirector();
   
-}
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
